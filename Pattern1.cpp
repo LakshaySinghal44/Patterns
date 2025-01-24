2e2dfdce94ba59d33b03ce424eb1a206c8ffcbc1
Lakshay Singhal <lakshay44singhal@gmail.com>
2025-01-24 09:43:39 +0530

    Update Pattern1.cpp

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main()
 {
    int n;
+   cout << "Enter the value of n : " ;
    cin >> n;
+   
+   if(n <=1){
+   cout << "Enter a valid number" << endl;}
+   
+   else{
    
    int i=1;
    
@@ -18,5 +24,7 @@ int main()
        cout << endl;
        i = i+1;
    }
-   
+   }
+   return 0;
 }
+
